0b8fcd3371564ad0762e0097e7d9128e96fa687e
Jonas <jonas.costa@icloud.com>
2015-09-08 14:11:40 -0300

    commit

---
+++ src/testes/Teste_interface.java
@@ -0,0 +1,13 @@
+package testes;
+
+import classes.Gerente;
+import interfaces.Promocao;
+
+public class Teste_interface {
+
+	public static void main(String[] args) {
+		Promocao promo = new Gerente();
+
+	}
+
+}
